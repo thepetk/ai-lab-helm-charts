99d8f426bd0479d9e2c735325fb9afaf2c47c6d7
thepetk <thepetk@gmail.com>
2025-01-29 14:52:39 +0000

    Make app-config permanent

--- generate.sh
+++ generate.sh
@@ -3,4 +3,4 @@
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # pull and convert resources from ai-lab-app
-bash $ROOTDIR/scripts/convert-gitops-template.sh
+bash "${ROOTDIR}/scripts/convert-gitops-template.sh"
